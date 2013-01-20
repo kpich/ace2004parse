f6a5a70788ecdf76f9cec7030d6a22ee5c5fb833
Karl Pichotta <karlpichotta@gmail.com>
2013-01-20 12:58:17 -0600

    working junit framework

---
+++ src/test/edu/utexas/cs/ml/ace2004parse/ACE2004ParseTest.java
@@ -0,0 +1,22 @@
+package edu.utexas.cs.ml.ace2004parse;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ACE2004ParseTest {
+
+  @BeforeClass
+  public static void testSetup(){
+  }
+
+  @AfterClass
+  public static void testCleanup(){
+  }
+
+  @Test
+  public void testTrue() {
+    assertTrue(true);
+    int a = 1 / 0;
+  }
+
+}
